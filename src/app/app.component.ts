c789e3a4048486403125f4b66e3f6f006cb9c749
appscalex2 <appscalex2@gmail.com>
2025-09-02 17:46:21 +0530

    Update app.component.ts

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,4 +8,8 @@ import { Component } from '@angular/core';
 export class AppComponent {
   title = 'sample-angular-app';
   myVariable = 123   
+
+   unusedFunction() {  
+    console.log('hello');
+  }
 }
